9042a318f800e4e8b992fcb180084be1f143687a
r0lm0 <rolmo92@gmail.com>
2025-09-03 07:00:13 -0600

    Fix typo in MessagePattern for findAllProducts command

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,7 +16,7 @@ export class ProductsController {
   }
 
   //@Get()
-  @MessagePattern({ cmd: 'find_all_product' })
+  @MessagePattern({ cmd: 'find_all_products' })
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
